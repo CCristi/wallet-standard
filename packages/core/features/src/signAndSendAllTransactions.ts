27bfd8c74dbe6d257f4d69acd4a6fc61212ba276
Cristian Covali <christian@phantom.app>
2024-11-19 18:39:53 +0200

    feat: Add sign and send all transactions feature

---
+++ packages/core/features/src/signAndSendAllTransactions.ts
@@ -0,0 +1,45 @@
+import type { SolanaTransactionVersion } from './signTransaction.js';
+import type {
+    SolanaSignAndSendTransactionInput,
+    SolanaSignAndSendTransactionMode,
+    SolanaSignAndSendTransactionOutput,
+} from './signAndSendTransaction.js';
+
+/** Name of the feature */
+export const SignAndSendAllTransactions = 'solana:signAndSendAllTransactions';
+
+/** TODO: docs */
+export type SolanaSignAndSendAllTransactionsFeature = {
+    /** Name of the feature. */
+    readonly [SignAndSendAllTransactions]: {
+        /** Version of the feature API. */
+        readonly version: SolanaSignAndSendAllTransactionsVersion;
+
+        /** TODO: docs */
+        readonly supportedTransactionVersions: readonly SolanaTransactionVersion[];
+
+        /**
+         * Sign transactions using the account's secret key and send them to the chain.
+         *
+         * @param inputs Inputs for signing and sending transactions.
+         *
+         * @return Outputs of signing and sending transactions.
+         */
+        readonly signAndSendTransaction: SolanaSignAndSendAllTransactionsMethod;
+    };
+};
+
+/** Version of the feature. */
+export type SolanaSignAndSendAllTransactionsVersion = '1.0.0';
+
+/** TODO: docs */
+export type SolanaSignAndSendAllTransactionsMethod = (
+    inputs: readonly SolanaSignAndSendTransactionInput[],
+    options?: SolanaSignAndSendAllTransactionsOptions
+) => Promise<readonly PromiseSettledResult<SolanaSignAndSendTransactionOutput>[]>;
+
+/** Options for signing and sending multiple transactions. */
+export type SolanaSignAndSendAllTransactionsOptions = {
+    /** Mode for signing and sending transactions. */
+    readonly mode?: SolanaSignAndSendTransactionMode;
+};
